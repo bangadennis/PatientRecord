dac8107b6216b6d869885391d5298f279177d5fb
banga <dennisbanga@gmail.com>
2015-05-07 08:51:41 +0300

    Added class members and setters/getters to model PatientDrugs

--- src/main/java/com/patientrecord/models/PatientDrugs.java
+++ src/main/java/com/patientrecord/models/PatientDrugs.java
@@ -1,7 +1,46 @@
 package com.patientrecord.models;
 
+import javax.persistence.*;
+
 /**
  * Created by banga on 5/7/15.
  */
+@Entity
+@Table(name = "patientdrugs")
 public class PatientDrugs {
+    @Id
+    @GeneratedValue
+    private int pk;
+
+    @Column(name = "patient_id")
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private int patientId;
+
+    @Column(name="drug_name")
+    private String drugName;
+
+    public int getPk() {
+        return pk;
+    }
+
+    public void setPk(int pk) {
+        this.pk = pk;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
 }
