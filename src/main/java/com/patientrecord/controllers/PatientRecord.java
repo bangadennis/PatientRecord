6365273bf32c29e92c0f8f334f2c53ce6ed4d490
banga <dennisbanga@gmail.com>
2015-05-07 08:33:16 +0300

    Added class members and getters/setters for model PatientDetails

--- src/main/java/com/patientrecord/controllers/PatientRecord.java
+++ src/main/java/com/patientrecord/controllers/PatientRecord.java
@@ -1,9 +1,44 @@
 package com.patientrecord.controllers;
 
+import javax.persistence.*;
+
 /**
  * Created by banga on 5/7/15.
  */
+@Entity
+@Table(name = "patientdrugs")
 public class PatientRecord {
+    @Id
+    @GeneratedValue
+    private int pk;
 
+    @Column(name="patient_id")
+    private int patientId;
 
+    @Column(name="drug_name")
+    private String drugName;
+
+    public int getPk() {
+        return pk;
+    }
+
+    public void setPk(int pk) {
+        this.pk = pk;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
 }
--- src/main/java/com/patientrecord/models/PatientDetails.java
+++ src/main/java/com/patientrecord/models/PatientDetails.java
@@ -1,7 +1,68 @@
 package com.patientrecord.models;
 
+
+import javax.persistence.*;
+import java.security.Timestamp;
+import java.util.Date;
+
 /**
  * Created by banga on 5/7/15.
  */
+@Entity
+@Table(name = "patientdetials")
 public class PatientDetails {
+    @Id
+    @GeneratedValue
+    private int patientId;
+
+
+    private String fname;
+
+    private String lname;
+
+    @Column(name = "dob")
+    private Date dateofbirth;
+
+    private String timestamp;
+
+    //getters and setters
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public Date getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(Date dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
